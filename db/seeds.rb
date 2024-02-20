a19b77f2a282fbdcf437376f47c8cb31cca505ba
LuchoAravena <lucho.sg2016@gmail.com>
2024-02-20 17:14:45 -0300

    Seed was completed to test API

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create(email: 'lucho@gmail.com', password: 'password1', password_confirmation: 'password1')
+User.create(email: 'usuario2@example.com', password: 'password2', password_confirmation: 'password2')
+
+Wallet.create(user_id: 1, currency: 'USD', amount: 52000)
+Wallet.create(user_id: 1, currency: 'BTC', amount: 0.00000253)
+
+Transaction.create(user_id: 1, currency_sent: 'USD', currency_received: 'BTC', amount_sent: 48.00, amount_received: 0.0004258)
